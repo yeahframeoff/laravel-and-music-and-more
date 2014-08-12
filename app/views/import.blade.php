20aace99643602332ade74cc859ceda7a19b4aa6
W01fdale <vershinin.a@donnu.edu.ua>
2014-08-12 17:31:22 +0000

    import page fix

--- app/views/import.blade.php
+++ app/views/import.blade.php
@@ -13,10 +13,12 @@
         @if(isset($socials['fb']))
         	<a href="{{ URL::to('/import/fb') }}">{{ HTML::image('public/images/FB_logo_big.png', 'Facebook', array('title' => 'Импорт музыки из Facebook')) }}</a>
         @else
-        	<a href="{{ URL::to('/connect/fb') }}">{{ HTML::image('public/images/FB_logo_big.png', 'Facebook', array('class' => 'inactive')) }}</a>
-            <div class="connect">
-                Подключить
-            </div>
+        	<a href="{{ URL::to('/connect/fb') }}">
+                {{ HTML::image('public/images/FB_logo_big.png', 'Facebook', array('class' => 'inactive')) }}
+                <div class="connect">
+                    Подключить
+                </div>
+            </a>            
         @endif
     </span>
     
@@ -24,10 +26,12 @@
         @if(isset($socials['vk']))
         	<a href="{{ URL::to('/import/vk') }}">{{ HTML::image('public/images/VK_logo_big.png', 'Facebook', array('title' => 'Импорт музыки из Vkontakte')) }}</a>
         @else
-        	<a href="{{ URL::to('/connect/vk') }}">{{ HTML::image('public/images/VK_logo_big.png', 'Vkontakte', array('class' => 'inactive')) }}</a>
-        	<div class="connect">
-                Подключить
-            </div>
+            <a href="{{ URL::to('/connect/vk') }}">
+                {{ HTML::image('public/images/VK_logo_big.png', 'Vkontakte', array('class' => 'inactive')) }}
+                <div class="connect">
+                    Подключить
+                </div>
+            </a>   
         @endif
     </span>
     
@@ -35,10 +39,12 @@
         @if(isset($socials['ok']))
         	<a href="{{ URL::to('/import/ok') }}">{{ HTML::image('public/images/OK_logo_big.png', 'Facebook', array('title' => 'Импорт музыки из Одноклассники')) }}</a>
         @else
-        	<a href="{{ URL::to('/connect/ok') }}">{{ HTML::image('public/images/OK_logo_big.png', 'Одноклассники', array('class' => 'inactive')) }}</a>
-            <div class="connect">
-                Подключить
-            </div>
+        	<a href="{{ URL::to('/connect/ok') }}">
+                {{ HTML::image('public/images/OK_logo_big.png', 'Одноклассники', array('class' => 'inactive')) }}
+                <div class="connect">
+                    Подключить
+                </div>
+            </a>   
         @endif
     </span>
 </div>
