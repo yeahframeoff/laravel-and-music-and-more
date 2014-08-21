ddb63119cc8846446537703d39ecd1da10de6198
yeahframeoff <anthyef@gmail.com>
2014-08-21 11:28:21 +0300

    0020: Search engine now supports checking relations too

--- app/src/Search.php
+++ app/src/Search.php
@@ -4,17 +4,39 @@ namespace Karma\Util;
 
 class Search
 {
-    public static function search($className, $fields, $string)
+    public static function search($string, $className, $fields, array $related = array())
     {
         $attrs = self::resolveSearchString($string);
         $result = $className::getModel();
+        $fields = self::resolveArrayParam($fields);
+
         foreach ($fields as $field)
-        {
             foreach ($attrs as $attr)
-                $result = $result->orWhere($field, 'LIKE', '%'.$attr.'%');
+                $result = $result->orWhere($field, 'LIKE', '%' . $attr . '%');
+
+        $withArray = array();
+        foreach ($related as $relation => $relFields)
+        {
+            if (!is_array($relFields))
+                $relFields = self::resolveArrayParam($relFields);
+            $withArray[$relation] = function($query) use ($relFields, $attrs)
+            {
+                foreach ($relFields as $field)
+                    foreach ($attrs as $attr)
+                        $query->orWhere($field, 'LIKE', '%' . $attr . '%');
+            };
         }
+        if (!empty ($withArray))
+            $result = $result->with($withArray);
         $result = $result->get();
         return $result;
+    }
+
+    private static function resolveArrayParam($param)
+    {
+        if (!is_array($param))
+            $param = array($param);
+        return $param;
     }
     
     private static function resolveSearchString($arg)
