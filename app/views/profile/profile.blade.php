e3ecd2f620cacaa4779488fda073943f6cba7b86
yeahframeoff <anthyef@gmail.com>
2014-09-05 11:48:05 +0300

    0025: profile view refactored

---
+++ app/views/profile/profile.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.main')
+
+@section('stylesheets')
+    {{ HTML::style('public/css/player.css') }}
+    @parent
+@stop
+
+@section('scripts')
+    @parent
+    {{ HTML::script('public/js/audioController.js') }}
+@stop
+
+@section('content')
+<div style="margin: auto 0;">
+    <span class="glyphicon glyphicon-user"></span>&nbsp;
+    <h1 style="display: inline;">{{ $user->first_name . ' ' . $user->last_name  }}</h1>
+
+</div>
+
+<hr>
+<div class="row">
+    <div class="col-md-6">
+        @include ('profile.icon_span', ['user' => $user])
+        @include ('profile.audio_span', ['user' => $user])
+    </div>
+
+    <div class="col-md-6">
+        @include ('profile.friends_span', ['user' => $user])
+        <hr>
+        @include ('profile.groups_span', ['user' => $user])
+        <hr>
+        @include ('profile.feed_span', ['posts' => $user->receivedPosts])
+    </div>
+</div>
+
+@stop
