58253eccf894091abdcc5674e760459c4abd08da
yeahframeoff <anthyef@gmail.com>
2014-09-18 18:17:41 +0300

    0030: refactored

--- app/views/navbar/logged_only.blade.php
+++ app/views/navbar/logged_only.blade.php
@@ -33,24 +33,35 @@
     </ul>
 </li>
 
-<li @if(Request::is('/friends*')) class="active" @endif >
-    @if ($count = \KAuth::user()->friendshipz()->requests()->count() > 0)
-        <div class="btn-group btn-group-sm" style="margin: 10% auto">
-            <a class="btn btn-primary" href="{{ URL::route('friends.my') }}">
-                <strong>Друзья</strong>
-            </a>
-            <a class="btn btn-default" href="{{ URL::route('friends.my') }}?p=requests">
-                <strong>+{{$count}}</strong>
-            </a>
-        </div>
-    @else
-        <a href="{{ URL::route('friends.my') }}">Друзья</a>
-    @endif
 
-</li>
 
 <li @if(Request::is('/groups'))class="active"@endif><a href="{{ URL::to('groups') }}">Группы</a></li>
-<li @if(Request::is('/messages'))class="active"@endif><a href="{{ URL::to('messages') }}">Диалоги</a></li>
 <li @if(Request::is('/library'))class="active"@endif><a href="{{ URL::to('library') }}">Библиотека</a></li>
-<li @if(Request::is('/profile'))class="active"@endif><a href="{{ URL::to('profile') }}">Профиль</a></li>
+
+<li class="dropdown">
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+        Личное
+        <b class="caret"></b>
+    </a>
+
+    <ul class="dropdown-menu">
+        <li><a href="{{ URL::to('profile') }}">Профиль</a></li>
+        <li><a href="{{ URL::to('messages') }}">Диалоги</a></li>
+        <li>
+            @if ($count = \KAuth::user()->friendshipz()->requests()->count() > 0)
+                <div class="btn-group btn-group-sm" style="margin: 10% auto">
+                    <a class="btn btn-primary" href="{{ URL::route('friends.my') }}">
+                        <strong>Друзья</strong>
+                    </a>
+                    <a class="btn btn-default" href="{{ URL::route('friends.my') }}?p=requests">
+                        <strong>+{{$count}}</strong>
+                    </a>
+                </div>
+            @else
+                <a href="{{ URL::route('friends.my') }}">Друзья</a>
+            @endif
+        </li>
+    </ul>
+</li>
+
 <li><a href="{{ URL::to('/logout') }}">Выйти</a></li>
