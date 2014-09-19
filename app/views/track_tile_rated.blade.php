8c35f49633f665dc9fb03dcdcf6d99484a14ce25
yeahframeoff <anthyef@gmail.com>
2014-09-19 17:58:03 +0300

    fix rated songs

--- app/views/track_tile_rated.blade.php
+++ app/views/track_tile_rated.blade.php
@@ -1,8 +1,9 @@
 <h3><strong>{{ $track->track->artist->name }}</strong>&nbsp;-&nbsp;{{ $track->track->title }}&nbsp;
     <a href="#" class="addTrack" deezer="{{isset($deezer)}}"
        data-src="{{$track->track_url}}" data-cover="{{$track->track->albums->first()->artwork or ''}}">
-        <span class="glyphicon glyphicon-plus"></span>
+
     </a>
+    <span class="glyphicon glyphicon-plus"></span>
     <div class="raty-rated" data-rated-type="track" data-id="{{ $track->track->id }}"
         data-href="{{ URL::route('rate.rate') }}"></div>
 </h3>
